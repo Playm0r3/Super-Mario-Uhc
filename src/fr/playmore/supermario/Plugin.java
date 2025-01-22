607c49ca43563a41407ac481927d055fa6b16b11
Playm0r3 <warren.macabre@proton.me>
2025-01-22 20:30:08 +0100

    [INIT] Initialisation du projet
    Les fichiers principaux pour que le plugin soit détecté par un serveur Minecraft.

---
+++ src/fr/playmore/supermario/Plugin.java
@@ -0,0 +1,12 @@
+package fr.playmore.supermario;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		System.out.print("Serveur en marche...");
+	}
+
+}
